a5f862737cf44bb2ccdf2ed2f8d877962b4d4879
Gareth <gareth@garethveale.dev>
2020-04-06 08:07:40 +0100

    modified website title

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `GARETH VEALE`,
     author: {
       name: `Gareth Veale.`,
-      summary: `Software engineer 💻. Business neophyte. Full-time flaneur. Pushing myself daily 💪.`,
+      summary: `Software engineer 💻. Business and technology enthusiast. Full-time flaneur. Focusing on striving & propsering.`,
     },
     description: `Gareth Veale.`,
     siteUrl: `https://garethveale.dev/`,
