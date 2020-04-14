d656065507ede9180dbe3238cb18ac4151a01d8c
Gareth <gareth@garethveale.dev>
2020-04-14 16:03:49 +0100

    posts page groups posts per category

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -6,38 +6,77 @@ import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
 
 const BlogIndex = ({ data, location }) => {
+  let tagPostData = {}
   const siteTitle = data.site.siteMetadata.title
-  const posts = data.allMarkdownRemark.edges
+  const posts     = data.allMarkdownRemark.edges
+  
+  // get all unique tags from each post
+  let tags = []
+  posts.forEach((post, _) => {
+    if (post.node.frontmatter.tags) {
+      tags = tags.concat(post.node.frontmatter.tags)
+    }
+  })
+
+  const getUnique = arrArg => {
+    return arrArg.filter((elem, pos, arr) => {
+      return arr.indexOf(elem) === pos
+    })
+  }
+
+  tags = getUnique(tags)
+
+  const getPostsWithTag = (posts, tag) => {
+    let res = []
+    posts.forEach((post, _)=> {
+      if (post.node.frontmatter.tags.includes(tag)) {
+        res.push(post)
+      }
+    })
+    return res;
+  }
+
+  // build a data structure to hold tags : posts
+  tags.forEach(tag => {
+    tagPostData[tag] = getPostsWithTag(posts, tag);
+  })
 
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" />
       <Bio location={location}/>
-
-      {posts.map(({ node }) => {
-        const title = node.frontmatter.title || node.fields.slug
+      {Object.entries(tagPostData).map(([key, value]) =>{
+        const posts = value.map(({ node }) => {
+          const title = node.frontmatter.title || node.fields.slug
+          return (
+            <article key={node.fields.slug}>
+              <header>
+                <h3
+                  style={{
+                    marginBottom: rhythm(1 / 4),
+                  }}
+                >
+                  <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                    {title}
+                  </Link>
+                </h3>
+                <small>{node.frontmatter.date} • <span role="img" aria-label="coffee">☕</span> {node.frontmatter.minread} min read</small>
+              </header>
+              <section>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: node.frontmatter.description || node.excerpt,
+                  }}
+                />
+              </section>
+            </article>
+          )
+        })
         return (
-          <article key={node.fields.slug}>
-            <header>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date} • <span role="img" aria-label="coffee">☕</span> {node.frontmatter.minread} min read</small>
-            </header>
-            <section>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-              />
-            </section>
-          </article>
+          <div>
+            <h2>{key}</h2>
+            {posts}
+          </div>
         )
       })}
     </Layout>
