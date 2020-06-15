d4905004db301bcd8498ef5b00f0be3ae44e92d0
Gareth Veale <gareth@garethveale.dev>
2020-06-15 19:53:49 +0100

    Updates

---
+++ component---src-pages-index-tsx-9d2ba0e1e4e5da74eef8.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{QeBL:function(e,t,a){"use strict";a.r(t),a.d(t,"pageQuery",(function(){return o}));var n=a("q1tI"),i=a.n(n),r=a("Bl7J"),c=a("vrFN");t.default=function(e){var t=e.data,a=e.location,n=t.site.siteMetadata.title;return i.a.createElement("div",{style:{alignItems:"center",justifyContent:"center"}},i.a.createElement(r.a,{location:a,title:n},i.a.createElement(c.a,{title:"Home"})))};var o="1097489062"}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-9d2ba0e1e4e5da74eef8.js.map
