44f4f89872dc60d0efedf848a4dd476e078ea688
garethiv <gareth@garethveale.dev>
2021-02-01 09:15:41 +0000

    change bio

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `GARETH VEALE.`,
     author: {
       name: `Gareth Veale.`,
-      summary: `Writing code, building things, training my body. Student of everything. Documenting my journey. Read my notes about technology, business, and health. Or read what I wrote about some books I read.`,
+      summary: `Writing code, and words. Student of everything. Read what I wrote in Notes, or read what I wrote about some books I read.`,
     },
     description: `Gareth Veale.`,
     siteUrl: `https://garethveale.dev/`,
